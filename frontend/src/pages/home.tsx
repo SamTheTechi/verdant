b1d7e635f0868626f959b0c45f625defed1bc7f4
SamTheProBot <gsameer478@gmail.com>
2024-06-04 03:36:08 +0530

    Initial

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>Hello therer</div>;
+};
+
+export default Home;
---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+
+const Login = () => {
+  const [input, setInput] = useState();
+
+  useEffect;
+
+  return (
+    <>
+      <section className='w-[100vw] h-[100vh] bg-red-500'>hello</section>
+    </>
+  );
+};
+
+export default Login;
